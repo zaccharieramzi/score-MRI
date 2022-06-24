20ab8730172a74acad2cc875d1eef0aaf3f117e1
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-06-24 14:45:29 +0200

    added tests for the mc scripts

---
+++ tests/test_inference_multi_coil_hybrid.py
@@ -0,0 +1,16 @@
+from unittest.mock import patch
+
+from score_mri.inference_multi_coil_hybrid import main
+
+
+def test_inference_multi_coil_hybrid():
+    args = [
+        "main",
+        "--data", "001",
+        "--mask_type", "gaussian1d",
+        "--acc_factor", "4",
+        "--center_fraction", "0.08",
+        "--N", "2",
+    ]
+    with patch("sys.argv", args):
+        main()
