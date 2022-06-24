e2df9db0c15627bc97eb2a45b17a4300f70b8f32
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-06-24 14:02:18 +0200

    added a test for inference real

---
+++ tests/test_inference_real.py
@@ -0,0 +1,20 @@
+import os
+
+from unittest.mock import patch
+import torch
+
+from score_mri.inference_real import main
+
+
+def test_inference_real(config):
+    args = [
+        "main",
+        "--task", "retrospective",
+        "--data", "001",
+        "--mask_type", "gaussian1d",
+        "--acc_factor", "4",
+        "--center_fraction", "0.08",
+        "--N", "2",
+    ]
+    with patch("sys.argv", args):
+        main()
